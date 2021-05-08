3d7bb99255354a62410c245e12284f9ed5ec741c
Code Sandbox <hello@codesandbox.io>
2021-05-08 23:37:03 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1457cefc9b982599d54065eca5d62844.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7560cdb9ec9f058bbdd5",
+    "url": "https://cystemerror.github.io/csb-72pso/static/css/main.2631e82c.chunk.css"
+  },
+  {
+    "revision": "7560cdb9ec9f058bbdd5",
+    "url": "https://cystemerror.github.io/csb-72pso/static/js/main.97b4a65b.chunk.js"
+  },
+  {
+    "revision": "dd8efa84a2466d6615ec",
+    "url": "https://cystemerror.github.io/csb-72pso/static/js/runtime~main.dc385175.js"
+  },
+  {
+    "revision": "498436a2537c777a451f",
+    "url": "https://cystemerror.github.io/csb-72pso/static/js/2.c4c9218c.chunk.js"
+  },
+  {
+    "revision": "45d25e7a4e6d63ca0e41a78f14a2bcfb",
+    "url": "https://cystemerror.github.io/csb-72pso/index.html"
+  }
+];
